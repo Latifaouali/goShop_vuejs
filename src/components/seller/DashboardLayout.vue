55f699fa12ae032e2de5c70ba61a54cd0281892c
Latifaouali <153641016+Latifaouali@users.noreply.github.com>
2023-12-14 15:29:38 +0100

    Update DashboardLayout.vue

--- src/components/seller/DashboardLayout.vue
+++ src/components/seller/DashboardLayout.vue
@@ -46,7 +46,7 @@
             aria-expanded="false"
           >
             <img
-              :src="`../../user/${user.picture}`"
+              :src="`https://goshop-3bs3.onrender.com/${user.picture}`"
               alt="Profile"
               class="rounded-circle"
             />
