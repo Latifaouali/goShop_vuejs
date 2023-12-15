1e24892937e32a4dc2e0bf529e34c721adff3d9f
Latifaouali <153641016+Latifaouali@users.noreply.github.com>
2023-12-15 11:13:52 +0100

    Update PageWishlist.vue

--- src/views/Users/PageWishlist.vue
+++ src/views/Users/PageWishlist.vue
@@ -49,7 +49,7 @@
                 <td style="display: table-cell">
                   <div class="d-flex align-items-center">
                     <div class="avatar avatar-sm">
-                      <img :src="`https://goshop-3bs3.onrender.com/${product.productID.picture} `" />
+                      <img :src="`${product.productID.picture} `" />
                     </div>
                     <h6 class="fs-sm mb-0 ms-2">
                       {{ product.productID.name }}
